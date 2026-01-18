eb2e148f58157d5d27b7bd38998744306148cf83
Prathamesh Wagh <135869300+PSW9102004@users.noreply.github.com>
2026-01-18 23:16:52 +0530

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,44 +1,18 @@
 from django import forms
-from django.utils.text import slugify
-from .models import Comment, Post, Tag
+from .models import Comment, Post  # <--- 'Tag' is removed
 
 class PostForm(forms.ModelForm):
+    # We can keep this field definition to make the UI look nice (placeholder, help text)
+    # django-taggit expects a simple text string, which this provides.
     tags = forms.CharField(
         required=False,
         help_text='Comma-separated tags (e.g. design, startup, ai).',
         widget=forms.TextInput(attrs={'placeholder': 'design, startup, ai'})
     )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            self.fields['tags'].initial = ', '.join(
-                self.instance.tags.values_list('name', flat=True)
-            )
-
     class Meta:
         model = Post
         fields = ['title', 'subtitle', 'content', 'status', 'tags']
-
-    def clean_tags(self):
-        raw_tags = self.cleaned_data.get('tags', '')
-        tags = [tag.strip() for tag in raw_tags.split(',') if tag.strip()]
-        return tags
-
-    def save(self, commit=True):
-        post = super().save(commit=commit)
-        if commit:
-            tags = self.cleaned_data.get('tags', [])
-            tag_objects = []
-            for tag in tags:
-                slug = slugify(tag)[:50] or tag.lower().replace(' ', '-')
-                tag_obj, _ = Tag.objects.get_or_create(slug=slug, defaults={'name': tag})
-                if tag_obj.name != tag:
-                    tag_obj.name = tag
-                    tag_obj.save(update_fields=['name'])
-                tag_objects.append(tag_obj)
-            post.tags.set(tag_objects)
-        return post
 
 class CommentForm(forms.ModelForm):
     class Meta:
